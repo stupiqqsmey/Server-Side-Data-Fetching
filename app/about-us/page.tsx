83ec5cd14c9e40fe58846de39457323958f0c480
unknown <lonhraksmey828282@gmail.com>
2026-03-05 23:44:16 +0700

    [--feat: added login form and insert validation--]

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,13 +1,6 @@
-import Link from 'next/link';
-
-const Navbar = () => {
-  return (
-    <nav>
-      <Link href="/about">
-        About Us
-      </Link>
-    </nav>
-  );
+const AboutPage = () => {
+  return <div>About Us Content</div>
 };
 
+export default AboutPage; 
 
